502ed81612902b254ef23cf136425d62da91c817
Mesalcode <57722542+Mesalcode@users.noreply.github.com>
2023-08-14 13:02:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("test123")
+import speech_recognition as sr
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data, language="de-DE")
+    print(text)
