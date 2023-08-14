971fbacde69cf88b3997a6b026965e443265681c
Mesalcode <57722542+Mesalcode@users.noreply.github.com>
2023-08-14 13:14:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@
 
 import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+while True:
 
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language="de-DE"))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Warte auf Frage..")
+        audio = r.listen(source)
+
+    try:
+        print("Erkannter Satz: " + r.recognize_google(audio, language="de-DE"))
+    except sr.UnknownValueError:
+        print("Dies konnte nicht verstanden werden.")
+    except sr.RequestError as e:
+        print("Dies konnte nicht verarbeitet werden.")
