635716691f320c8108c9714680fc26646f28f705
Mesalcode <57722542+Mesalcode@users.noreply.github.com>
2023-08-17 13:51:34 +0200

    Update hardware_server.py

--- hardware_server.py
+++ hardware_server.py
@@ -5,6 +5,13 @@ ser = serial.Serial('COM4', 9600, timeout=1)
 while True:
     line = ser.readline()
 
-    print(line)
+    line_split = line.split(' ')
+
+    if len(line_split) != 2:
+        continue
+
+    temperature = float(line_split[0])
+    humidity = float(line_split[1])
+    print(f"temp: {temperature} humid: {humidity}")
 
 ser.close()
